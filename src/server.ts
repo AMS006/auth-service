604c8b0fd23f11928c3631bb69965b0716ee1e6f
Anas5006 <anassain13@gmail.com>
2024-02-11 12:22:14 +0530

    Remove server file from sonar check

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,43 @@ import app from './app';
 import { Config } from './config';
 import { AppDataSource } from './config/data-source';
 import logger from './config/logger';
-import { createAdminUser } from './utils';
+import bcrypt from 'bcryptjs';
+import { User } from './entity/User';
+import { Roles } from './constants';
+
+export const createAdminUser = async () => {
+    const { ADMIN_EMAIL, ADMIN_PASSWORD } = Config;
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        if (userRepository) {
+            const isAdminPresent = await userRepository.findOne({
+                where: { email: ADMIN_EMAIL },
+            });
+
+            if (isAdminPresent) {
+                logger.info('Admin user already exists');
+                return;
+            }
+        }
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(ADMIN_PASSWORD!, saltRounds);
+
+        return await userRepository.save({
+            firstName: 'Admin',
+            lastName: 'User',
+            email: ADMIN_EMAIL,
+            password: hashedPassword,
+            role: Roles.ADMIN,
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            logger.error(error.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
+    }
+};
 
 const startServer = async () => {
     const PORT = Config.PORT;
